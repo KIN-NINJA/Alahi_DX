ccc916ec427f15a97bf2d5649aec55d566597fe0
KIN-NINJA <149870026+KIN-NINJA@users.noreply.github.com>
2023-12-11 23:30:41 +0600

    Add files via upload

--- ala.py
+++ ala.py
@@ -86,8 +86,8 @@ def cek_apk(session,coki):
         print(f'\r[🔥] %s \x1b[1;95m Your Active Apps     :{GREEN}'%(GREEN))
         for i in range(len(game)):
             print(f"\033[\033[1;92m[%s%s] %s%s"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
-        #else:
-            #print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
+        else:
+            print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
     w=session.get("https://free.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
     sop = BeautifulSoup(w,"html.parser")
     x = sop.find("form",method="post")
@@ -183,7 +183,7 @@ logo="""
 \033[1;91m[-] s¥ѕ𝕋𝔢𝓜    :\033[1;32mĐ₳₮₳ & ₩ł₣ł
 \033[1;35m[-] 𝔾丨Ŧ𝓱υ𝐛   :\033[1;91m𝔸𝕷𝞐Нⵏ
 \033[1;36m[-] 𝔽𝔸ℂ𝔼𝔹𝕆𝕆𝕂  :\033[1;36m\x1b[1;33mF\x1b[1;34ma\x1b[1;36mz\x1b[1;35ml\x1b[1;90ma\x1b[1;91m A\x1b[1;92ml\x1b[1;93ma\x1b[1;94mh\x1b[1;95mi\x1b[1;96m
-\033[1;91m[-] 𝐕𝔼𝐑𝐬𝕚𝕆𝐍   :\033[1;33m\x1b[1;33m5\x1b[1;96m.\x1b[1;31m5\x1b[1;34m
+\033[1;91m[-] 𝐕𝔼𝐑𝐬𝕚𝕆𝐍   :\033[1;33m\x1b[1;33m5\x1b[1;96m.\x1b[1;36m6\x1b[1;34m
 \033[1;92m══════════════════════════════════════════
 \033[1;91m<═══\033[1;41m\033[1;96m THIS NAME IS \033[1;35m\x1b[1;33mF̺͆\x1b[1;34ma̺͆\x1b[1;36mz̺͆\x1b[1;35ml̺͆\x1b[1;90ma̺͆\x1b[1;91m A̺͆\x1b[1;92ml̺͆\x1b[1;93ma̺͆\x1b[1;94mh̺͆\x1b[1;95mi̺͆\x1b[1;96mBRAND\033[;0m\033[1;91m═══>\033[1;92m"""
 
@@ -354,11 +354,11 @@ def rcrack1(uid,pwx,tl):
             'path':'/home.php?tbua=1',
             'scheme': 'https',    
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-BD,en;q=0.9,bn-BD;q=0.8,bn;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+            'accept-language': 'en-BD,en;q=0.9,bn-BD;q=0.8,bn;q=0.7,en-GB;q=0.6,en-US;q=0.5,en-US,en;q=0.9',
             'cache-control': 'no-cache, no-store, must-revalidate',
             'pragma': 'no-cache',
             'referer': 'https://x.facebook.com/',
-            'sec-ch-ua': '"Chromium";v="120", "Not=A?Brand";v="8"',
+            'sec-ch-ua': '"Chromium";v="116", "Not=A?Brand";v="99"',
             'sec-ch-ua-mobile': '?1',
             'sec-ch-ua-platform': '"Android"',
             'sec-fetch-dest': 'document',
@@ -484,11 +484,11 @@ def rcrack1(uid,pwx,tl):
             'path':'/home.php?tbua=1',
             'scheme': 'https',    
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-BD,en;q=0.9,bn-BD;q=0.8,bn;q=0.7,en-GB;q=0.6,en-US;q=0.5',
+            'accept-language': 'en-BD,en;q=0.9,bn-BD;q=0.8,bn;q=0.7,en-GB;q=0.6,en-US;q=0.5,en-US,en;q=0.9',
             'cache-control': 'no-cache, no-store, must-revalidate',
             'pragma': 'no-cache',
             'referer': 'https://x.facebook.com/',
-            'sec-ch-ua': '"Chromium";v="120", "Not=A?Brand";v="8"',
+            'sec-ch-ua': '"Chromium";v="116", "Not=A?Brand";v="99"',
             'sec-ch-ua-mobile': '?1',
             'sec-ch-ua-platform': '"Android"',
             'sec-fetch-dest': 'document',
